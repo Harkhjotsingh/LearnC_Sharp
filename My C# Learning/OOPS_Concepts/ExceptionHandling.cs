1ef118c366f9572abb9f6a3544e194bb7549d4d6
harkhjotsingh <harkhjotsingh@gmail.com>
2017-01-25 16:16:04 -0500

    Exception Handling Example and Notes

---
+++ My C# Learning/OOPS_Concepts/ExceptionHandling.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+
+namespace MyNameSpace
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            StreamReader readThisFile = null;
+
+            try
+            {
+                readThisFile = new StreamReader(@"C:\Users\harry\Desktop\exitNotesPalette.txt");
+                Console.WriteLine(readThisFile.ReadToEnd());
+            }
+            catch (FileNotFoundException excep)
+            {
+                Console.WriteLine("EXP1: Cannot find file: " + excep.FileName);
+            }
+            catch (DirectoryNotFoundException excep)
+            {
+                Console.WriteLine("EXP2: " + excep.Message);
+            }
+            catch (Exception excep)
+            {
+                Console.WriteLine("EXP3: " + excep.Message);            
+            }
+            finally
+            {
+                if(readThisFile != null)
+                    readThisFile.Close();
+                Console.WriteLine("Finally");
+            }
+            Console.Read();
+        }
+    }
+}
+
+
+/*
+    :::::::::::::::::EXCEPTION HANDLING::::::::::::::::
+
+    Whenever we have an unforseen error that the application was not expecting, we get an Exception. (for ex: while opening a file we expect the file to be present)
+    A good application should be capable of handling exception. In above code we are trying to open a file and read it in console application. But what if the file 
+    name/path is not correct or if someone has changed it. Similarly while reading from the database if the table is not there or something like that goes wrong, our
+    program should be capable of handling such exceptions.
+
+    First lets see WHAT IS AN EXCEPTION??
+    -> An exception is a class that derives from System.Exception Class under System base class library. There are other Exception classes that are specification of the 
+    Exception class like FileNotFoundException,DirectoryNotFoundException etc. Each of these classes directly or indirectly inherits from Exception class.
+    
+    ******IMP:::: Exception class is super base class for all other specific exception classes
+    
+    WHY WE SHOOULD HANDLE EXCEPTIONS???
+    -> Its annoying for end user to see them, as they are not familier with the technical language it may be in. 
+    -> It stops or crashes the application.
+    -> It holds on to resources which must be released.
+    -> It gives valuable information to hackers who can use it to hack into the application.
+    
+    Now to handle an exception we can use something like try-catch-finally. 
+    Syntax:  try{
+                    // Something that might give an exception. Our program:: Open a file in our case. -->> line 14
+                }
+           catch{
+                    // What to do if exception occurs, like log data to log files or database.  Our program:: Give out a meaningful message. -->> line 19 and 23
+                }
+         finally{
+                    // Do this regardless there is an exception or not. Our program:: free up resources(close StreamReader). -->> line 31 and 32.
+                }
+    
+    So even if there is an exception we are not holding on to resources and not giving out valueable information to the bad guys.  
+
+
+    NOTE:: The class Exception is most generic exception class and covers all the exceptions that could occur. But the information about the exception it provides is 
+    limited. Therfore any special exception handling must be called before calling general exception handling. 
+    For example: in above code example we called FileNotFoundException and DirectoryNotFoundException class exceptions before calling Exception class object. Not doing so 
+    will give a compilation error.
+
+
+*/
